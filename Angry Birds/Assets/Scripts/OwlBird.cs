811071337a16ca096ff87532aec819ae6f78a311
MIrfanKamal <irfan.kamal12@gmail.com>
2020-09-11 22:29:56 +0700

    Project

---
+++ Angry Birds/Assets/Scripts/OwlBird.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    public float blastRadius;
+    public float blastTime;
+    public SpriteRenderer spriteRenderer;
+
+    // Ketika bertabrakan dengan obstacle atau enemy
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string tag = collision.gameObject.tag;
+        if(tag == "Enemy" || tag == "Obstacle")
+        {
+            RigidBody.velocity = Vector2.zero;
+            spriteRenderer.enabled = false;
+            RigidBody.bodyType = RigidbodyType2D.Kinematic;
+            StartCoroutine(Blast());
+        }
+    }
+
+    // Meledak dengan membesarkan collider
+    IEnumerator Blast()
+    {
+        Collider.radius = blastRadius;
+        yield return new WaitForSeconds(blastTime);
+        Destroy(gameObject);
+    }
+}
